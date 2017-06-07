6ba7b838b35bca02472ddc89993641a45f955e8f
pumpe zhu <zzplxm1988@gmail.com>
2017-06-07 12:46:36 +0800

    修改打包输出规则

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,10 @@ var path = require('path')
 var webpack = require('webpack')
 module.exports = {
   entry: {
-  	'wechat.accout.binding.min': path.resolve('src', 'index.js')
+  	'wechat.accout.binding': path.resolve('src', 'index.js')
   },
   output: {
-    filename: '[name].[hash:7].js',
+    filename: '[name].[hash:7].min.js',
     path: path.resolve(__dirname, 'dist')
   }
 }
